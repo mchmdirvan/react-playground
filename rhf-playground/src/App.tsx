c41f44f3d341cfc2600b02f5718fc3b92da4d5d2
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-07-09 16:48:45 +0700

    feat: create tanstack playground

--- rhf-playground/src/App.tsx
+++ rhf-playground/src/App.tsx
@@ -1,3 +1,29 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+const schema = z.object({
+  name: z.string(),
+  age: z.number(),
+});
+
+type Schema = z.infer<typeof schema>;
+
 export default function App() {
-  return <div>App</div>;
+  const { register, handleSubmit } = useForm({
+    resolver: zodResolver(schema),
+  });
+
+  return (
+    <form
+      onSubmit={handleSubmit((data) => {
+        // handle inputs
+        console.log(data);
+      })}
+    >
+      <input {...register("name")} />
+      <input {...register("age", { valueAsNumber: true })} type="number" />
+      <input type="submit" />
+    </form>
+  );
 }
