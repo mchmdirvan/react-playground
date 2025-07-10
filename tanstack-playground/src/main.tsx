1ec24b8fc27149858e10b3a7775bf24210b20764
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-07-10 10:02:20 +0700

    feat: implement basic table, cell formating, row actions

--- tanstack-playground/src/main.tsx
+++ tanstack-playground/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./app.tsx";
+// import App from "./app.tsx";
+import DemoPage from "./payments/page.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <DemoPage />
   </StrictMode>
 );
