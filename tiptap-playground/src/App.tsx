39e5a2243768dbb90663ee5c02504324e8f54f19
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-07-21 11:46:35 +0700

    choreL add starter kit tiptap

--- tiptap-playground/src/App.tsx
+++ tiptap-playground/src/App.tsx
@@ -1,10 +1,9 @@
-import { Button } from "./components/ui/button";
+import Tiptap from "./components/tiptap";
 
 export default function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button>Click me</Button>
+      <Tiptap />
     </div>
   );
 }
---
+++ tiptap-playground/src/components/tiptap.tsx
@@ -0,0 +1,23 @@
+// src/Tiptap.tsx
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+
+// define your extension array
+const extensions = [StarterKit];
+
+const content = "<p>Hello World!</p>";
+
+const Tiptap = () => {
+  const editor = useEditor({
+    extensions,
+    content,
+  });
+
+  return (
+    <>
+      <EditorContent editor={editor} />
+    </>
+  );
+};
+
+export default Tiptap;
