58be43b710d710fb4241bbc3d1d3b34624d7ffea
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-07-21 09:23:13 +0700

    chore: tidy up

--- tiptap-playground/src/App.tsx
+++ tiptap-playground/src/App.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import { Button } from "./components/ui/button";
 
 export default function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button>Click me</Button>;
+      <Button>Click me</Button>
     </div>
   );
 }
