4e5df28419e4ace457ce35e166a6941781fe58f7
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-07-31 09:25:09 +0700

    feat: create dynamic input

--- dynamic-input/src/App.tsx
+++ dynamic-input/src/App.tsx
@@ -1,5 +1,62 @@
-import React from "react";
+import { useState } from "react";
 
-export default function App() {
-  return <div>Hello</div>;
+function App() {
+  const [formFields, setFormFields] = useState([{ name: "", age: "" }]);
+
+  const handleFormChange = (event, index) => {
+    const data = [...formFields];
+    data[index][event.target.name] = event.target.value;
+    setFormFields(data);
+  };
+
+  const submit = (event) => {
+    event.preventDefault();
+    console.log(formFields);
+  };
+
+  const addFields = () => {
+    const object = {
+      name: "",
+      age: "",
+    };
+
+    setFormFields([...formFields, object]);
+  };
+
+  const removeFields = (index) => {
+    const data = [...formFields];
+    data.splice(index, 1);
+    setFormFields(data);
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={submit}>
+        {formFields.map((form, index) => {
+          return (
+            <div key={index}>
+              <input
+                name="name"
+                placeholder="Name"
+                onChange={(event) => handleFormChange(event, index)}
+                value={form.name}
+              />
+              <input
+                name="age"
+                placeholder="Age"
+                onChange={(event) => handleFormChange(event, index)}
+                value={form.age}
+              />
+              <button onClick={() => removeFields(index)}>Remove</button>
+            </div>
+          );
+        })}
+      </form>
+      <button onClick={addFields}>Add More..</button>
+      <br />
+      <button onClick={submit}>Submit</button>
+    </div>
+  );
 }
+
+export default App;
